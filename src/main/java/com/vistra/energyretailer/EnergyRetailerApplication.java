859740bcf85ff3a46f56ecca3f6df8727dad1f3a
Robert <jose.villafuerte@3pillarglobal.com>
2022-10-04 11:49:11 -0600

    First commit

---
+++ src/main/java/com/vistra/energyretailer/EnergyRetailerApplication.java
@@ -0,0 +1,13 @@
+package com.vistra.energyretailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyRetailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyRetailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vistra/energyretailer/EnergyRetailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vistra.energyretailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyRetailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
